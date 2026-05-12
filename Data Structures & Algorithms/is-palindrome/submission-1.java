1d87c1004704f65141e834e862978046d8232403
Ashok_dev <112305148+Ashok-461999@users.noreply.github.com>
2026-05-12 17:12:18 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String str = s.replaceAll("[^a-zA-Z0-9]", "");
+        int start = 0;
+        int end = str.length() - 1;
+
+        str = str.toLowerCase();
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end))
+                return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
